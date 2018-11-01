1bbbddeb7d05d984494933d48bd492be050961ed
Tejas Bhosale <bhosaletejas93@gmail..com>
2018-11-01 11:16:00 +0530

    add store form and handling the submit

--- controllers/storeContoller.js
+++ controllers/storeContoller.js
@@ -1,3 +1,5 @@
+
+
 exports.myMiddleWare = (req, res,next) => {
     req.name = 'wes';
     next(); // here the next symbolises that the work was done by this middleware go to next middleware.
@@ -6,4 +8,14 @@ exports.myMiddleWare = (req, res,next) => {
 exports.homePage = (req, res) => {
     console.log(req.name);
     res.render('index');
+}
+
+exports.addStore = (req, res) => {
+    res.render('editStore' , {
+        title: 'Add Store'
+    });
+}
+
+exports.createStore = (req, res) => {
+    res.json(req.body);
 }
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,8 @@ const storeController = require('../controllers/storeContoller');
 // Do work here
 // we first pass it to my middleware and then as a next parameter we pass it to homePage.
 router.get('/', storeController.myMiddleWare , storeController.homePage);
-
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 // 
 router.get('/reverse/:name', (req, res) => {
   const reversed = [...req.params.name].reverse().join('');
