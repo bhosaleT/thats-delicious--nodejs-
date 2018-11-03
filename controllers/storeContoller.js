5bdee700b906faeb83e76c60b682f0f8925cde9a
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-11-03 13:58:00 +0530

    adding images and resizing them

--- controllers/storeContoller.js
+++ controllers/storeContoller.js
@@ -1,6 +1,26 @@
 const mongoose = require("mongoose");
 // we will use the singelton property of mongoose to get the store.
 const Store = mongoose.model("Store");
+const multer = require("multer");
+const jimp = require("jimp");
+const uuid = require("uuid");
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith("image/");
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next(
+        {
+          message: `That filetype isn\'t allowed!`
+        },
+        false
+      );
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
   console.log(req.name);
@@ -12,6 +32,25 @@ exports.addStore = (req, res) => {
     title: "Add Store"
   });
 };
+
+exports.upload = multer(multerOptions).single("photo");
+
+exports.resize = async (req, res, next) => {
+  // check if there is no new file to resize
+  if (!req.file) {
+    next(); // skip to the next middleware.
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // now we resize;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write('./public/uploads/${req.body.photo}');
+  // once we have written the photo to our filesystem, keep going!
+  next();
+};
+
 // Adding the keyword async tells the browser that the function that I AM writing is gowing to have some
 //awaits in it.
 exports.createStore = async (req, res) => {
@@ -41,12 +80,17 @@ exports.editStore = async (req, res) => {
 exports.updateStore = async (req, res) => {
   // Set the store location to be a point.
   req.body.location.type = "Point";
-    //Find and update the store.
-    const store = await Store.findOneAndUpdate({_id : req.params.id}, req.body , {
-        new: true,
-        runValidators: true,
-    }).exec();
-    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}"> View Store -></a>` )
-    //redirect to say that the store was successfully updated.
-    res.redirect(`/stores/${store._id}/edit`);
-}
+  //Find and update the store.
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true,
+    runValidators: true
+  }).exec();
+  req.flash(
+    "success",
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }"> View Store -></a>`
+  );
+  //redirect to say that the store was successfully updated.
+  res.redirect(`/stores/${store._id}/edit`);
+};
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const storeController = require('../controllers/storeContoller');
+const storeController = require("../controllers/storeContoller");
 
-const { catchErrors } = require('../handlers/errorHandlers');
+const { catchErrors } = require("../handlers/errorHandlers");
 
 // Do work here
 // we first pass it to my middleware and then as a next parameter we pass it to homePage.
-router.get('/',  catchErrors(storeController.getStores));
-router.get('/stores',  catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
-router.post('/add', catchErrors( storeController.createStore));
-router.post('/add/:id', catchErrors( storeController.updateStore));
-// 
-router.get('/reverse/:name', (req, res) => {
-  const reversed = [...req.params.name].reverse().join('');
+router.get("/", catchErrors(storeController.getStores));
+router.get("/stores", catchErrors(storeController.getStores));
+router.get("/add", storeController.addStore);
+router.post(
+  "/add",
+  storeController.upload,
+  catchErrors(storeController.resize),
+  catchErrors(storeController.createStore)
+);
+router.post("/add/:id", catchErrors(storeController.updateStore));
+//
+router.get("/reverse/:name", (req, res) => {
+  const reversed = [...req.params.name].reverse().join("");
   res.send(reversed);
-})
+});
 
-router.get('/stores/:id/edit' , catchErrors(storeController.editStore));
+router.get("/stores/:id/edit", catchErrors(storeController.editStore));
 
 module.exports = router;
