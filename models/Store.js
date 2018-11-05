f50e36cc8e2985dc398bf4f5da5d55ccd918614a
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-11-05 18:34:13 +0530

    fixed the different slugs with same name issue

--- models/Store.js
+++ models/Store.js
@@ -1,51 +1,59 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //we set the promise in mongoose to the es6 async await promise.
 mongoose.Promise = global.Promise;
-const slug = require('slugs');
+const slug = require("slugs");
 
 const storeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "Please enter a store name"
+  name: {
+    type: String,
+    trim: true,
+    required: "Please enter a store name"
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now()
+  },
+  location: {
+    type: {
+      type: String,
+      default: "Point"
     },
-    slug: String,
-    description: {
-        type: String,
-        trim: true
-    },
-    tags: [String],
-    created: {
-        type: Date,
-        default: Date.now()
-    },
-    location: {
-        type: {
-            type: String,
-            default: 'Point'
-        },
-        coordinates: [{
-            type: Number,
-            required: "You must supply coordinates"
-        }],
-        address: {
-            type: String,
-            required: "You must supply an address"
-        }
-    },
-    photo: String
-});
-
-
-storeSchema.pre('save', function (next) {
-    //we only want this to run when the stores name is changed.
-    if (!this.isModified('name')) {
-        next();
-        return;
+    coordinates: [
+      {
+        type: Number,
+        required: "You must supply coordinates"
+      }
+    ],
+    address: {
+      type: String,
+      required: "You must supply an address"
     }
-    this.slug = slug(this.name);
-    next();
-    // TODO make more detailed to make slugs UNIQUE.
+  },
+  photo: String
 });
 
-module.exports = mongoose.model('Store', storeSchema);
+storeSchema.pre("save", async function(next) {
+  //we only want this to run when the stores name is changed.
+  if (!this.isModified("name")) {
+    next();
+    return;
+  }
+  this.slug = slug(this.name);
+  // find othere stores that have a slug of new, new-1, new-2.
+  // we  are looking for a slug that starts with slug and ends with - or numbers 0-9
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, "i");
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
+  next();
+  // TODO make more detailed to make slugs UNIQUE.
+});
+
+module.exports = mongoose.model("Store", storeSchema);
