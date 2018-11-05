ea457f0dd3907f699d21f4d1b16650cef88c8d16
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-11-05 19:17:34 +0530

    added the tag pug

--- models/Store.js
+++ models/Store.js
@@ -56,4 +56,12 @@ storeSchema.pre("save", async function(next) {
   // TODO make more detailed to make slugs UNIQUE.
 });
 
+storeSchema.statics.getTagsList = function() {
+  return this.aggregate([
+    { $unwind: "$tags" },
+    { $group: { _id: "$tags", count: { $sum: 1 } } },
+    { $sort: {count: -1} }
+  ]);
+};
+
 module.exports = mongoose.model("Store", storeSchema);
