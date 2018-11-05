345a8fc5ce1b5092eb7c62c5b246238aa23dcbf5
Tejas Bhosale <bhosaletejas93@gmail.com>
2018-11-05 20:09:43 +0530

    filtering stores based on tag

--- controllers/storeContoller.js
+++ controllers/storeContoller.js
@@ -101,13 +101,16 @@ exports.getStoreBySlug = async (req, res, next) => {
   if (!store) {
     return next();
   }
-  res.render('store', {store , title: store.name})
+  res.render("store", { store, title: store.name });
 };
 
 //Getting stores by tags.
-exports.getStoresByTag = async (req,res) =>{
- const tags = await Store.getTagsList();
- const tag = req.params.tag;
-res.render('tag', {tags, title:"Tags", tag} )
-
-}
+exports.getStoresByTag = async (req, res) => {
+  const tag = req.params.tag;
+  const tagQuery = tag || {$exists: true};
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+  const [tags, stores] =await Promise.all([tagsPromise, storesPromise]);
+ 
+  res.render('tag', {tags, title:"Tags", tag, stores} )
+};
